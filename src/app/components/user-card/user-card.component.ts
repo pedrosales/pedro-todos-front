bc3d78ff9e868b9b6471fdf6cf5cdf8515af4865
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-17 20:59:25 -0300

    login e logout finalizados

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -6,12 +6,22 @@ import { AngularFireAuth } from '@angular/fire/auth';
   styleUrls: ['./user-card.component.css']
 })
 export class UserCardComponent implements OnInit {
+  public user: any = {
+    name: "",
+    picture: "https://placehold.it/200"
+  };
 
   constructor(
     private afAuth: AngularFireAuth
   ) { }
 
   ngOnInit(): void {
+    this.afAuth.user.subscribe((data) => {
+      if (data) {
+        this.user.name = data.displayName;
+        this.user.picture = data.photoURL;
+      }
+    });
   }
 
   logout() {
