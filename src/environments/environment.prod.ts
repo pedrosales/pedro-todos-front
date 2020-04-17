968fd251dbd47ac0a57a4d6ec3794fc80f9c6b77
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-17 20:04:58 -0300

    instalando e configurando firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBU5ABf16Qa2Uj-qiNGdUpXnz8_IflDxqo",
+    authDomain: "todoapp7199.firebaseapp.com",
+    databaseURL: "https://todoapp7199.firebaseio.com",
+    projectId: "todoapp7199",
+    storageBucket: "todoapp7199.appspot.com",
+    messagingSenderId: "224568230422",
+    appId: "1:224568230422:web:1b3adf0035af2f0e03ab37"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBU5ABf16Qa2Uj-qiNGdUpXnz8_IflDxqo",
+    authDomain: "todoapp7199.firebaseapp.com",
+    databaseURL: "https://todoapp7199.firebaseio.com",
+    projectId: "todoapp7199",
+    storageBucket: "todoapp7199.appspot.com",
+    messagingSenderId: "224568230422",
+    appId: "1:224568230422:web:1b3adf0035af2f0e03ab37"
+  }
 };
 
 /*
